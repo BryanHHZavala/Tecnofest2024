5cf6fdd515e905825e3f8d5d1f0bcface2805ef6
BryanHHZavala <zavalarodriguez88@gmail.com>
2024-06-08 18:14:38 -0600

    firebase y env

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const Swal = require('sweetalert2');
 const path = require("path");
 const mysql = require("mysql");
 const admin = require("firebase-admin");
+require('dotenv').config();
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+};
 
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://tecnofest2024-default-rtdb.firebaseio.com/'
 });
 
